ab9de3e4e57603bde7c10957144216e62aa00eca
efecanzaim <efcnzm1@gmail.com>
2026-02-28 16:36:44 +0300

    PageView should fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -179,6 +179,23 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessJsonLd) }}
         />
+        {/* Meta Pixel Code */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '26407586398877003');
+          fbq('track', 'PageView');
+        `}} />
+        <noscript dangerouslySetInnerHTML={{ __html: `
+          <img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=26407586398877003&ev=PageView&noscript=1" />
+        `}} />
         <style dangerouslySetInnerHTML={{
           __html: `
             @font-face {
@@ -221,32 +238,6 @@ export default function RootLayout({
             gtag('config', '${GA_MEASUREMENT_ID}');
           `}
         </Script>
-        {/* Meta Pixel Code */}
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            if(!window._fbPixelLoaded){
-              window._fbPixelLoaded=true;
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '26407586398877003');
-              fbq('track', 'PageView');
-            }
-          `}
-        </Script>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=26407586398877003&ev=PageView&noscript=1"
-          />
-        </noscript>
         <Providers>
           <Header />
           <main className="min-h-screen">{children}</main>
